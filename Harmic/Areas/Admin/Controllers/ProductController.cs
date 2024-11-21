43321dcfcf12e081db6ff16eef5d806f4b3fa30a
PhhamThe <phamthe11a8@example.com>
2024-11-21 09:07:42 +0700

    the

---
+++ Harmic/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Harmic.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Harmic.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly BanHangHoaQuaContext _context;
+
+        public ProductController(BanHangHoaQuaContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Title,Alias,CategoryProductId,Levels,ParentId,Position,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy,IsActive")] TbMenu tbMenu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tbMenu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tbMenu);
+        }
+        public IActionResult Edit(int? id)
+        {
+            var menu = _context.TbMenus.Where(m => m.MenuId == id).FirstOrDefault();
+            return View(menu);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("MenuId,Title,Alias,Description,Levels,ParentId,Position,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy,IsActive")] TbMenu tbMenu)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(tbMenu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tbMenu);
+        }
+
+        // GET: Admin/Menus/Delete/5
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            var menu = _context.TbMenus.Where(m => m.MenuId == id).FirstOrDefault();
+            return View(menu);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, [Bind("MenuId,Title,Alias,Description,Levels,ParentId,Position,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy,IsActive")] TbMenu tbMenu)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Remove(tbMenu.MenuId = id);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tbMenu);
+        }
+    }
+}
